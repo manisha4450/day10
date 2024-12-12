97748720d1df3406fa1fcf41f7db8c7f44227645
manisha4450 <chowdaryc226@gmail.com>
2024-12-12 14:58:12 +0530

    Add files via upload

---
+++ any1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class any1{
+    public static void main(String[] args){
+        ArrayList<Integer>a=new ArrayList<>();
+        a.add(898);
+        a.add(89);
+        a.add(10);
+        System.out.println(a);
+        a.remove(2);
+        System.out.println(a);
+        a.clear();
+        System.out.println(a);
+    }
+}
