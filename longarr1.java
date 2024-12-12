ccb43d129882564f6283024b023df33d2d743c7a
manisha4450 <chowdaryc226@gmail.com>
2024-12-12 14:41:41 +0530

    Add files via upload

---
+++ longarr1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class longarr1{
+    public static void main(String[] args){
+        long ans=39;
+        Scanner sc=new Scanner(System.in);
+        long s=sc.nextLong();
+        System.out.println(ans);
+        System.out.println(s);
+    }
+}
