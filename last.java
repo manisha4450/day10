a075b53a55afb7fae0eaab733adc20b00541daa8
manisha4450 <chowdaryc226@gmail.com>
2024-12-12 15:14:54 +0530

    Add files via upload

---
+++ last.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class last{
+    public static void main(String[] args){
+        ArrayList<Integer>a=new ArrayList<>();
+        a.add(898);
+        a.add(89);
+        a.add(10);
+        System.out.println(a);
+        a.addFirst(39);
+        System.out.println(a);
+        a.addLast(19);
+        System.out.println(a);
+    }
+}
